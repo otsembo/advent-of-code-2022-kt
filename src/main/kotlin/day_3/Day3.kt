3142391cfeffb9f5c0aea451f240bb808a468eca
Ian Okumu <okumu.otsembo@gmail.com>
2022-12-10 17:02:47 +0300

    DAY 3: Completed

--- src/main/kotlin/day_3/Day3.kt
+++ src/main/kotlin/day_3/Day3.kt
@@ -6,25 +6,57 @@ fun dayThree(): Int{
 
     var score = 0
 
-    File("src/main/kotlin/day_3/input.txt").forEachLine { it ->
+    var group = 1
+
+    var elfGroup = emptyList<String>().toMutableList()
+
+    File("src/main/kotlin/day_3/input.txt").forEachLine {
         // line metadata
-        val size = it.length
-        val mid = size / 2
-        val p1 = it.substring(0, mid)
-        val p2 = it.substring(mid)
+//        val size = it.length
+//        val mid = size / 2
+//        val p1 = it.substring(0, mid)
+//        val p2 = it.substring(mid)
+
+        elfGroup.add(it)
+
+        // make collection
+        if (group == 3){
+
+            val commonCharacters = emptySet<Char>().toMutableSet()
+            elfGroup[0].forEach { C ->
+                if(elfGroup[1].contains(C) && elfGroup[2].contains(C)){
+                    commonCharacters.add(C)
+                }
+            }
+
+            commonCharacters.forEach {cc ->
+                score += calculateWeight(cc)
+            }
+
+            // reset trial
+            group = 1
+            elfGroup = emptyList<String>().toMutableList()
+
+        }else{
+
+            group++
+        }
+
 
         // common characters in set
-        val commonChars: MutableSet<Char> = emptySet<Char>().toMutableSet()
-
-        p1.toCharArray().forEach { c: Char ->
-            if (p2.contains(c)){
-                commonChars.add(c)
-            }
-        }
-
-        commonChars.forEach {cc ->
-            score += calculateWeight(cc)
-        }
+//        val commonChars: MutableSet<Char> = emptySet<Char>().toMutableSet()
+//
+//
+//
+//        p1.toCharArray().forEach { c: Char ->
+//            if (p2.contains(c)){
+//                commonChars.add(c)
+//            }
+//        }
+//
+//        commonChars.forEach {cc ->
+//            score += calculateWeight(cc)
+//        }
 
 
     }
