51c35460b4a0cec0884216ff67c61047c7f0b8c5
Ian Okumu <okumu.otsembo@gmail.com>
2022-12-13 19:03:24 +0300

    DAY 6: Optimized loop

--- src/main/kotlin/day_6/Day6.kt
+++ src/main/kotlin/day_6/Day6.kt
@@ -10,10 +10,10 @@ fun daySix(): Int{
     val input = File("src/main/kotlin/day_6/input.txt").readLines()[0]
 
     // loop through index and character
-    for ((index, character) in input.withIndex()){
+    for (index in input.indices){
         val endInd = if((14 + index ) <= input.length) (13 + index) else (input.length - 1)
         val sub = input.substring(index .. endInd)
-        val subSet = sub.toCharArray().toMutableSet()
+        val subSet = sub.toCharArray().toSet()
         if(subSet.size == sub.length){
             return (index + subSet.size)
         }
